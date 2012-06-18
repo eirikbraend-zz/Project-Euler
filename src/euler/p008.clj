7ba0b0c7a976795533592a2294814b67ce219b93
Eirik Braend <eirik.braend@gmail.com>
2012-06-18 23:45:35 +0200

    p8, p11

--- src/euler/p008.clj
+++ src/euler/p008.clj
@@ -1,5 +1,7 @@
 (ns euler.p008)
 
+; Find the greatest product of five consecutive digits in the 1000-digit number.
+
 (def input (partition 5 1 (slurp "resources/p008-input.txt")))
 
 (defn multiply [x]
