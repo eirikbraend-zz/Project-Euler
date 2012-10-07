45c3701537a6cc2adccd30ffca740085d70a2fe5
Eirik Braend <eirik.braend@gmail.com>
2012-10-08 00:14:40 +0200

    p19

--- src/euler/p019.clj
+++ src/euler/p019.clj
@@ -1,7 +1,5 @@
 (ns euler.p019)
 
-;You are given the following information, but you may prefer to do some research for yourself.
-;
 ;1 Jan 1900 was a Monday.
 ;Thirty days has September,
 ;April, June and November.
@@ -11,3 +9,35 @@
 ;And on leap years, twenty-nine.
 ;A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 ;How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+(defn divisible? [dividend divisor]
+  (zero? (mod dividend divisor)))
+
+(defn leap-year? [year]
+  (and 
+    (divisible? year 4) 
+    (or 
+      (not (divisible? year 100))
+      (divisible? year 400))))
+
+(defn create-dates [year]
+  (concat 
+    (month-dates 31)
+    (month-dates (if (leap-year? year)
+                   29
+                   28))
+    (month-dates 31) 
+    (month-dates 30) 
+    (month-dates 31) 
+    (month-dates 30) 
+    (month-dates 31) 
+    (month-dates 31) 
+    (month-dates 30) 
+    (month-dates 31) 
+    (month-dates 30) 
+    (month-dates 31)))
+
+(def century (mapcat create-dates (range 1901 2001)))
+
+
+
