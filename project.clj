1a6d96d966e4c9564ac1c0cf4e2e4f19f10a81c0
Eirik Braend <eirik.braend@gmail.com>
2012-10-11 00:11:38 +0200

    p028

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject euler "1.0.0-SNAPSHOT"
   :descriptio "Solutions to Project Euler"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/math.combinatorics "0.0.2"]])
+                 [org.clojure/math.combinatorics "0.0.2"]
+                 [org.clojure/math.numeric-tower "0.0.1"]])
