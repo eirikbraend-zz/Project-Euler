62175eccc3b92def3020a73c69e1f389332a6a01
Eirik Brænd <eirik.braend@gmail.com>
2013-09-25 12:48:15 +0200

    version bump clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject euler "1.0.0-SNAPSHOT"
   :descriptio "Solutions to Project Euler"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.combinatorics "0.0.3"]
                  [org.clojure/math.numeric-tower "0.0.2"]])
