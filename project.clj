f01732f436c4b57c8b8cdd01f2aa136c405dd544
Eirik Brænd <eirik.braend@gmail.com>
2014-06-19 23:38:22 +0200

    clojure 1.6

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject euler "1.0.0-SNAPSHOT"
-  :descriptio "Solutions to Project Euler"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :description "Solutions to Project Euler"
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.combinatorics "0.0.3"]
-                 [org.clojure/math.numeric-tower "0.0.2"]])
+                 [org.clojure/math.numeric-tower "0.0.2"]])	
